77f15769cf1c09862e7585c33d1acfecefbf7fdd
Alexey Mogilyovkin <zeroicq@gmail.com>
2019-05-27 21:00:30 +1000

    40 with brackets

--- 40.rkt
+++ 40.rkt
@@ -11,11 +11,11 @@
                         (deriv (multiplicand exp) var))
           (make-product (deriv (multiplier exp) var)
                         (multiplicand exp)))]
-        [(exponentiation? exp) (make-product
-                          (exponent exp)
-                          (make-product
-                           (make-exponentiation (base exp) (make-sum (exponent exp) -1))
-                           (deriv (base exp) var)))]
+        ;[(exponentiation? exp) (make-product
+        ;                 (exponent exp)
+        ;                  (make-product
+        ;                   (make-exponentiation (base exp) (make-sum (exponent exp) -1))
+        ;                   (deriv (base exp) var)))]
         [else
          (error "unknown expression type" exp)]))
 
@@ -29,44 +29,45 @@
         [(=number? m1 1) m2]
         [(=number? m2 1) m1]
         [(and (number? m1) (number? m2)) (* m1 m2)]
-        [else (list '* m1 m2)]))
+        [else (list m1 '* m2)]))
 
-(define (sum? x) (and (pair? x) (eq? (car x) '+)))
+(define (sum? x) (and (pair? x) (eq? (cadr x) '+)))
 
 (define (make-sum a1 a2)
   (cond [(=number? a1 0) a2]
         [(=number? a2 0) a1]
         [(and (number? a1) (number? a2))
          (+ a1 a2)]
-        [else (list '+ a1 a2)]))
+        [else (list a1 '+ a2)]))
 
 ; cadr := (car (cdr x))
 ; addend= first element in sum
-(define (addend s) (cadr s))
+(define (addend s) (car s))
 
 ; caddr := (car (cdr (cdr v))).
 ; augend = second element in sum
 (define (augend s) (caddr s))
 
-(define (product? x) (and (pair? x) (eq? (car x) '*)))
-(define (exponentiation? x) (and (pair? x) (eq? (car x) '**)))
+(define (product? x) (and (pair? x) (eq? (cadr x) '*)))
+;(define (exponentiation? x) (and (pair? x) (eq? (car x) '**)))
 
-(define (make-exponentiation b e)
-  (cond [(=number? e 0) 1]
-        [(=number? e 1) b]
-        [(and (number? b) (number? e)) (expt b e)]
-        [else (list '** b e)]))
+;(define (make-exponentiation b e)
+;  (cond [(=number? e 0) 1]
+;        [(=number? e 1) b]
+;        [(and (number? b) (number? e)) (expt b e)]
+;        [else (list '** b e)]))
 
-(define (multiplier p) (cadr p))
+(define (multiplier p) (car p))
 (define (multiplicand p) (caddr p))
 
-(define (base e) (cadr e))
-(define (exponent e) (caddr e))
+;(define (base e) (cadr e))
+;(define (exponent e) (caddr e))
 
 (define (=number? exp num) (and (number? exp) (= exp num)))
 
+(deriv '(x + (3 * y)) 'x)
 
-(deriv '(+ (+ x x) (+ x x )) 'x)
+(deriv '(x + (3 * (x + (y + 2)))) 'x)
 
 
 
