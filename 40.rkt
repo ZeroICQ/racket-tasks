85f70144fea6926641b9fc7e9f49eaf9658572e0
Alexey Mogilyovkin <zeroicq@gmail.com>
2019-05-28 20:53:17 +1000

    40 wip

--- 40.rkt
+++ 40.rkt
@@ -16,6 +16,18 @@
 
 (define (variable? x) (symbol? x))
 
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+; возвращает true, если в списке есть оператор
+(define (find-op op seq)
+  (accumulate (lambda (a b) (or b (eq? a op))) false seq))
+
+(define (flatten x) (if (and (pair? x) (null? (cdr x))) (accumulate append '() x) x))
+
 (define (same-variable? v1 v2)
   (and (variable? v1) (variable? v2) (eq? v1 v2)))
 
@@ -26,7 +38,7 @@
         [(and (number? m1) (number? m2)) (* m1 m2)]
         [else (list m1 '* m2)]))
 
-(define (sum? x) (and (pair? x) (eq? (cadr x) '+)))
+(define (sum? x) (and (pair? x) (find-op '+ x)))
 
 (define (make-sum a1 a2)
   (cond [(=number? a1 0) a2]
@@ -35,34 +47,45 @@
          (+ a1 a2)]
         [else (list a1 '+ a2)]))
 
+; variant of fold-left
+(define (get-pre element seq)
+  (define (iter result rest)
+     (cond [(or (null? rest) (equal? (car rest) element)) result]
+           [else (iter (append result (list (car rest))) (cdr rest))]))
+   (iter '() seq))
+
+(define (get-post element seq)
+  (define (iter rest)
+     (if (equal? (car rest) element) (cdr rest) (iter (cdr rest))))
+   (iter seq))
 
 ; cadr := (car (cdr x))
-; addend= first element in sum
-(define (addend s) (car s))
+;; addend= first element in sum
+(define (addend s) (flatten (get-pre '+ s)))
 
 ; caddr := (car (cdr (cdr v))).
 ; augend = second element in sum
-(define (augend s) (caddr s))
+(define (augend s) (flatten (get-post '+ s)))
 
-(define (product? x) (and (pair? x) (eq? (cadr x) '*)))
-;(define (exponentiation? x) (and (pair? x) (eq? (car x) '**)))
-
-;(define (make-exponentiation b e)
-;  (cond [(=number? e 0) 1]
-;        [(=number? e 1) b]
-;        [(and (number? b) (number? e)) (expt b e)]
-;        [else (list '** b e)]))
+(define (product? x) (and (pair? x) (find-op '* x) (not (find-op '+ x))))
 
 (define (multiplier p) (car p))
 (define (multiplicand p) (caddr p))
 
-
 (define (=number? exp num) (and (number? exp) (= exp num)))
 
-(deriv '(x + x + x) 'x)
+;(deriv '(x + x + (x* 6)) 'x)
+(define a '(x + x + (x * 6)))
+(sum? a);t
+(product? a);f
+(define b '(x * x * (x + 6)))
+(sum? b);f
+(product? b);t
+
+(define c '(x * (x + b) * x + (x + 6)))
+(get-pre '+ c)
+(get-post '+ c)
+(addend c)
+(augend c)
 
 ;(deriv '(x + (3 * (x + (y + 2)))) 'x)
-
-
-
-
