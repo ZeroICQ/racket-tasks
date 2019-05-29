bb94486b58892fa293ebf6f2b6d0b0a37a2e35b4
Alexey Mogilyovkin <zeroicq@gmail.com>
2019-05-29 09:52:53 +0000

    40

--- 40.rkt
+++ 40.rkt
@@ -35,7 +35,8 @@
 ;                        (accumulate (lambda (a b) (if (pair? a) (append a b) (append (list a) b))) '() x)))
 
 
-(define (flatten x) (if (and (pair? x) (null? (cdr x))) (car x) x))
+(define (flat-one x) (if (and (pair? x) (null? (cdr x))) (car x) x))
+(define (flat x) (accumulate (lambda (a b) (if (pair? a) (append a b) (append (list a) b))) '() x))
 
 (define (same-variable? v1 v2)
   (and (variable? v1) (variable? v2) (eq? v1 v2)))
@@ -45,16 +46,21 @@
         [(=number? m1 1) m2]
         [(=number? m2 1) m1]
         [(and (number? m1) (number? m2)) (* m1 m2)]
-        [else (list m1 '* m2)]))
+        [else (remove-brackets-product m1 m2)]))
 
 (define (sum? x) (and (pair? x) (find-op '+ x)))
+(define (remove-brackets-product m1 m2)
+  (cond [(and (product? m1) (product? m2)) (append m1 '* m2)]
+        [(and (product? m1) (not (product? m2))) (append m1 (list '* ) (list m2))]
+        [(and (not (product? m1)) (product? m2)) (append (list m1) (list '*) m2)]
+        [else (append (list m1) (list '*) (list m2))]))
 
 (define (make-sum a1 a2)
   (cond [(=number? a1 0) a2]
         [(=number? a2 0) a1]
         [(and (number? a1) (number? a2))
          (+ a1 a2)]
-        [else (list a1 '+ a2)]))
+        [else (flat (list a1 '+ a2))]))
 
 ; variant of fold-left
 (define (get-pre element seq)
@@ -65,21 +71,21 @@
 
 (define (get-post element seq)
   (define (iter rest)
-     (if (equal? (car rest) element) (cdr rest) (iter (cdr rest))))
-   (iter seq))
+    (if (equal? (car rest) element) (cdr rest) (iter (cdr rest))))
+  (iter seq))
 
 ; cadr := (car (cdr x))
 ;; addend= first element in sum
-(define (addend s) (flatten (get-pre '+ s)))
+(define (addend s) (flat-one (get-pre '+ s)))
 
 ; caddr := (car (cdr (cdr v))).
 ; augend = second element in sum
-(define (augend s) (flatten (get-post '+ s)))
+(define (augend s) (flat-one (get-post '+ s)))
 
 (define (product? x) (and (pair? x) (find-op '* x) (not (find-op '+ x))))
 
-(define (multiplier p) (flatten (get-pre '* p)))
-(define (multiplicand p) (flatten (get-post '* p)))
+(define (multiplier p) (flat-one (get-pre '* p)))
+(define (multiplicand p) (flat-one (get-post '* p)))
 
 (define (=number? exp num) (and (number? exp) (= exp num)))
 
@@ -97,10 +103,22 @@
 ;(addend c)
 ;(augend c)
 
-;(define d '(x + 3 * x * (x + y) + z))
+(define d '(x + 3 * x * (x + y) + z))
 
 (deriv d 'x)
 (deriv d 'y)
 (deriv d 'z)
 
 (deriv 'y 'x)
+
+(define e '(x * x * x))
+(deriv e 'x)
+
+(deriv '(x * (x + y * z)) 'x)
+(deriv '(x * (x + y * z)) 'y)
+(deriv '(x * (x + y * z)) 'z)
+
+(deriv '(x + (x + x + x)) 'x)
+(deriv '(x + (x + x * x + x)) 'x)
+(deriv '((x * x) * (x * x)) 'x)
+
